5dc018406d62b6d9368d8c87e635d174bb6efbee
RymMess <rym.messaadi@gmail.com>
2019-11-23 07:09:59 +0100

    homework

--- src/main/java/com/ditrraacademy/travelagency/core/UserController.java
+++ src/main/java/com/ditrraacademy/travelagency/core/UserController.java
@@ -1,6 +1,9 @@
 package com.ditrraacademy.travelagency.core;
 
+import com.ditrraacademy.travelagency.utils.ErrorResponseModel;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,8 +14,19 @@ public class UserController {
     @Autowired
     UserRepository userRepository;
     @PostMapping("/user")
-    public void createUser(@RequestBody User user){
-        userRepository.save(user);
+    public ResponseEntity<?> createUser(@RequestBody User user){
+        if (user.getName() ==null)
+            return new  ResponseEntity<>(new ErrorResponseModel("user name required"),HttpStatus.BAD_REQUEST);
+
+        if (user.getName().length() < 3)
+            return new  ResponseEntity<>(new ErrorResponseModel("Wrong user name"),HttpStatus.BAD_REQUEST);
+
+
+        if (user.getAge() <= 0)
+            return new  ResponseEntity<>(new ErrorResponseModel("Wrong user age"),HttpStatus.BAD_REQUEST);
+
+        User userdatabase= userRepository.save(user);
+        return new ResponseEntity<>(userdatabase,HttpStatus.OK);
     }
     @GetMapping("/users")
     public List<User> getAllUsers (){
@@ -20,20 +34,40 @@ public class UserController {
        return userList;
     }
     @GetMapping("/user/{id}")
-    public User getUserById(@PathVariable int id){
+    public ResponseEntity<?> getUser(@PathVariable int id){
 
         Optional<User> userOptional = userRepository.findById(id);
+        if(!userOptional.isPresent()){
+            ErrorResponseModel errorResponseModel = new ErrorResponseModel("wrong user id");
+            return  new ResponseEntity<>(errorResponseModel , HttpStatus.BAD_REQUEST);
+        }
         User user = userOptional.get();
-        return user;
+        return new ResponseEntity<>(user,HttpStatus.OK);
     }
 
     @PutMapping("/user/{id}")
-    public void updateUser(@PathVariable int id, @RequestBody User updateUser){
+    public ResponseEntity<?>  updateUser(@PathVariable int id, @RequestBody User updateUser){
         Optional<User> userOptional = userRepository.findById(id);
-        User databaserUser = userOptional.get();
-        databaserUser.setName(updateUser.getName());
-        databaserUser.setAge(updateUser.getAge());
-        userRepository.save(databaserUser);
+        User userDatabase = userOptional.get();
+        userDatabase.setName(updateUser.getName());
+        userDatabase.setAge(updateUser.getAge());
+        if (userDatabase.getName() ==null)
+            return new  ResponseEntity<>(new ErrorResponseModel("user name required"),HttpStatus.BAD_REQUEST);
 
+        if (userDatabase.getName().length() < 3)
+            return new  ResponseEntity<>(new ErrorResponseModel("Wrong user name"),HttpStatus.BAD_REQUEST);
+
+        if (userDatabase.getAge() <= 0)
+            return new  ResponseEntity<>(new ErrorResponseModel("Wrong user age"),HttpStatus.BAD_REQUEST);
+
+
+        User userupdate = userRepository.save(userDatabase);
+        return new ResponseEntity<>(userupdate,HttpStatus.OK);
+    }
+
+    @DeleteMapping("/user/{id}")
+    public  void removeUser(@PathVariable int id){
+
+        userRepository.deleteById(id);
     }
 }
