49dac8d4a20c5bbd4713a0f95e7eb90fa3bc3d51
Wael Ben Taleb <waelben7@gmail.com>
2019-11-23 11:50:41 +0100

    Rim

--- src/main/java/com/ditrraacademy/travelagency/core/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.ditrraacademy.travelagency.core;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Integer> {
-}
